f0eb693ef64f543478ef7fa56bfbb8c2f9747286
Demonty Nicolas <Nicolas.Demonty@faymonville.com>
2026-03-06 13:28:40 +0100

    Add PWA icons and update Vite configuration
    - Added new PWA icons: pwa-192x192.png and pwa-512x512.png.
    - Updated Vite configuration to include the new icons in the PWA assets.

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'pwa-192x192.png', 'pwa-512x512.png'],
       manifest: {
         name: 'Kipit App',
         short_name: 'Kipit',
